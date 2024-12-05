4ff764d63c9343509aece380bebe24d6a75f87a0
chenqh19 <chenqh23@mit.edu>
2024-12-05 06:02:36 +0000

    update pipeline example

--- my_examples/pipelined_compression_example.cpp
+++ my_examples/pipelined_compression_example.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <omp.h>
 
 #include "qpl/qpl.h"
 #include "examples_utils.hpp" // for argument parsing function
@@ -46,10 +47,13 @@ auto main(int argc, char** argv) -> int {
     std::vector<uint8_t> reference(source_size, 7);
 
     // Shared buffer for job initialization
-    std::unique_ptr<uint8_t[]> job_buffer;
-    qpl_job *job;
+    std::unique_ptr<uint8_t[]> job0_buffer, job1_buffer;
+    qpl_job *job0;
+    qpl_job *job1;
     uint32_t size = 0;
     qpl_status status;
+    qpl_status status0;
+    qpl_status status1;
 
     // Initialize the shared job buffer
     status = qpl_get_job_size(execution_path, &size);
@@ -58,11 +62,19 @@ auto main(int argc, char** argv) -> int {
         return 1;
     }
 
-    job_buffer = std::make_unique<uint8_t[]>(size);
-    job = reinterpret_cast<qpl_job *>(job_buffer.get());
-    status = qpl_init_job(execution_path, job);
+    job0_buffer = std::make_unique<uint8_t[]>(size);
+    job0 = reinterpret_cast<qpl_job *>(job0_buffer.get());
+    status = qpl_init_job(execution_path, job0);
     if (status != QPL_STS_OK) {
-        std::cout << "Error " << status << " initializing job.\n";
+        std::cout << "Error " << status << " initializing job0.\n";
+        return 1;
+    }
+
+    job1_buffer = std::make_unique<uint8_t[]>(size);
+    job1 = reinterpret_cast<qpl_job *>(job1_buffer.get());
+    status = qpl_init_job(execution_path, job1);
+    if (status != QPL_STS_OK) {
+        std::cout << "Error " << status << " initializing job1.\n";
         return 1;
     }
 
@@ -70,43 +82,85 @@ auto main(int argc, char** argv) -> int {
     size_t chunk_size = source.size() / 4;
     uint32_t compressed_size[4] = {0};
 
-    for (int i = 0; i < 4; i++) {
-        job->op            = qpl_op_compress;
-        job->level         = qpl_default_level;
-        job->next_in_ptr   = source.data() + i * chunk_size;
-        job->next_out_ptr  = destination.data() + i * (chunk_size / 2); // Assume compression reduces size by half
-        job->available_in  = static_cast<uint32_t>(chunk_size);
-        job->available_out = static_cast<uint32_t>(destination.size() / 4);
-        job->flags         = QPL_FLAG_FIRST | QPL_FLAG_LAST | QPL_FLAG_DYNAMIC_HUFFMAN | QPL_FLAG_OMIT_VERIFY;
+    // Job descriptiors
+    job0->op            = qpl_op_compress;
+    job0->level         = qpl_default_level;
+    job0->flags         = QPL_FLAG_FIRST | QPL_FLAG_LAST | QPL_FLAG_DYNAMIC_HUFFMAN | QPL_FLAG_OMIT_VERIFY;
 
-        status = qpl_execute_job(job);
-        if (status != QPL_STS_OK) {
-            std::cout << "Error " << status << " during compression for chunk " << i << ".\n";
+    job1->op            = qpl_op_decompress;
+    job1->flags         = QPL_FLAG_FIRST | QPL_FLAG_LAST;
+
+    // First chunk first stage
+    job0->next_in_ptr   = source.data();
+    job0->next_out_ptr  = destination.data(); // Assume compression reduces size by half
+    job0->available_in  = static_cast<uint32_t>(chunk_size);
+    job0->available_out = static_cast<uint32_t>(destination.size() / 4);
+    
+    status = qpl_execute_job(job0);
+    if (status != QPL_STS_OK) {
+        std::cout << "Error " << status << " during compression for chunk " << 0 << ".\n";
+        return 1;
+    }
+    compressed_size[0] = job0->total_out;
+    
+    // TODO: make it a pipeline
+    for (int i = 1; i < 4; i++) {
+        // Decompress the chunk
+        
+        // vdestination.data() + i * (chunk_size / 2); // Offset to compressed chunk
+        job1->next_in_ptr   = destination.data() + (i-1) * (chunk_size / 2); // Why not job0->next_out_ptr?
+        job1->next_out_ptr  = reference.data() + (i-1) * chunk_size;
+        job1->available_in  = job0->total_out;
+        job1->available_out = static_cast<uint32_t>(chunk_size);
+
+        // Compress the chunk
+        job0->next_in_ptr   = source.data() + i * chunk_size;
+        job0->next_out_ptr  = destination.data() + i * (chunk_size / 2); 
+        job0->available_in  = static_cast<uint32_t>(chunk_size);
+        job0->available_out = static_cast<uint32_t>(destination.size() / 4);
+        
+        // TODO: Execute the jobs in parallel
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                status0 = qpl_execute_job(job1);
+            }
+            #pragma omp section
+            {
+                status1 = qpl_execute_job(job0);
+            }
+        }
+
+        if (status0 != QPL_STS_OK) {
+            std::cout << "Error " << status0 << " during decompression for chunk " << i-1 << ".\n";
             return 1;
         }
-        compressed_size[i] = job->total_out;
+
+        if (status1 != QPL_STS_OK) {
+            std::cout << "Error " << status1 << " during compression for chunk " << i << ".\n";
+            return 1;
+        }
+        compressed_size[i] = job0->total_out;
     }
 
-    // Decompression in 4 chunks using the same job buffer
-    for (int i = 0; i < 4; i++) {
-        job->op            = qpl_op_decompress;
-        job->next_in_ptr   = destination.data() + i * (chunk_size / 2); // Offset to compressed chunk
-        job->next_out_ptr  = reference.data() + i * chunk_size;         // Offset to decompressed chunk
-        job->available_in  = compressed_size[i];
-        job->available_out = static_cast<uint32_t>(chunk_size);
-        job->flags         = QPL_FLAG_FIRST | QPL_FLAG_LAST;
+    // Last chunk second stage
+    job1->next_in_ptr   = destination.data() + 3 * (chunk_size / 2);
+    job1->next_out_ptr  = reference.data() + 3 * chunk_size;         // Offset to decompressed chunk
+    job1->available_in  = job0->total_out;
+    job1->available_out = static_cast<uint32_t>(chunk_size);
 
-        status = qpl_execute_job(job);
-        if (status != QPL_STS_OK) {
-            std::cout << "Error " << status << " during decompression for chunk " << i << ".\n";
-            return 1;
-        }
+    status1 = qpl_execute_job(job1);
+    if (status1 != QPL_STS_OK) {
+        std::cout << "Error " << status1 << " during decompression for chunk " << 3 << ".\n";
+        return 1;
     }
 
     // Finalize the shared job
-    status = qpl_fini_job(job);
-    if (status != QPL_STS_OK) {
-        std::cout << "Error " << status << " finalizing job.\n";
+    status0 = qpl_fini_job(job0);
+    status1 = qpl_fini_job(job1);
+    if (status0 != QPL_STS_OK || status1 != QPL_STS_OK) {
+        std::cout << "Error " << status0 << status1 << " finalizing job.\n";
         return 1;
     }
 
